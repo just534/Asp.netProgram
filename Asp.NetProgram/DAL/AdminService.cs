3e4dccab0898314ed9e254c8bb6cbfab3ed287b4
just534 <273510287@qq.com>
2019-09-28 14:12:19 +0800

    添加项目文件。

---
+++ Asp.NetProgram/DAL/AdminService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.NetProgram.DAL
+{
+    public class AdminService
+    {
+        public bool AdminLogin(string name,string pwd)
+        {
+            if (name == "xiaowang" && pwd == "123456")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Asp.NetProgram/Handlers/Login.ashx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp.NetProgram.Handlers
+{
+    /// <summary>
+    /// Login 的摘要说明
+    /// </summary>
+    public class Login : IHttpHandler
+    {
+        private DAL.AdminService objAdminService = new DAL.AdminService();
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string uname = context.Request.Params["uname"];//获取传入的用户名
+            string upwd = context.Request.Params["upwd"];//获取传入的密码
+            if (objAdminService.AdminLogin(uname, upwd))
+            {
+                context.Response.Write("登陆成功");
+            }
+            else
+            {
+                context.Response.Write("登陆失败");
+            }
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
